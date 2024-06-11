61fea09c4ca613a6e50dd0912a8a653801149b70
nimondo <znimed@gmail.com>
2024-06-11 17:06:37 +0000

    Alter middleware conf

--- middleware/authrole.js
+++ middleware/authrole.js
@@ -10,9 +10,8 @@ const authRole = (role) => {
   return (req, res, next) => {
 
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const decodedToken = jwt.verify(token, process.env.TOKEN);
     const decodedRole = decodedToken.role;
-    console.log(role, roleData[decodedRole], decodedRole)
     if (!roleData[decodedRole].includes(role)) {
       res.status(401);
       console.log("not allowed");
